ac37929d31b24ef670aa02fea23e5a3cb314c601
zMathi <38568440+zmathi@users.noreply.github.com>
2019-09-07 13:49:14 -0300

    Just removing a line of code

--- src/com/github/zmathi/automaticator/timer/Timer.java
+++ src/com/github/zmathi/automaticator/timer/Timer.java
@@ -36,9 +36,7 @@ public class Timer {
                 if (command != null && !command.isEmpty()) {
                     if (command.contains("{player}")) {
                         for (Player player : Bukkit.getOnlinePlayers()) {
-                            command = command.replace("{player}", player.getName());
-
-                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("{player}", player.getName()));
                         }
                         continue;
                     }
